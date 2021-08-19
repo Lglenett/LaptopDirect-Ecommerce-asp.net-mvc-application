890a8bd31245271a4865a1af7a746491a723a3bb
Lesley Molepo <lglenett@gmail.com>
2021-08-19 08:15:41 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaptopDirect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
